6b570f303010f5cc8e63d8e09b9997939ac071fa
Erfan <soulburn007@gamil.com>
2020-03-05 21:09:13 +0330

    changed message of the day default value

--- src/components/Motd/Motd.js
+++ src/components/Motd/Motd.js
@@ -2,9 +2,7 @@ import React from 'react';
 import styles from './Motd.module.css';
 
 const Motd = props => {
-  const msg = props.error
-    ? props.error
-    : 'Sharing a few of our favorite movies';
+  const msg = props.error ? props.error : 'Type Something To Search ( •̀ ω •́ )y';
 
   return <div className={styles.Motd}>{msg}</div>;
 };
