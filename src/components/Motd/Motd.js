5e3b2a595cc79fe38148aa1c720dd5615086f47e
Erfan <soulburn007@gamil.com>
2020-03-05 23:20:17 +0330

    improved motd message

--- src/components/Motd/Motd.js
+++ src/components/Motd/Motd.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Motd.module.css';
 
 const Motd = props => {
-  const msg = props.error ? props.error : 'Type Something To Search ( •̀ ω •́ )y';
+  const msg = props.error
+    ? `${props.error} ＞﹏＜`
+    : props.totalResults
+    ? `I found ${props.totalResults} results (/≧▽≦)/`
+    : 'Type Something To Search ( •̀ ω •́ )/';
 
   return <div className={styles.Motd}>{msg}</div>;
 };
